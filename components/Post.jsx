40d6c942f9c6b5262a4b3e5f6e6c22cd844f1bbe
Leo <morimoto.ke@gmail.com>
2021-04-03 23:03:06 +0900

    ♻️ デフォルトインポートを削除

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Post({ post }) {
+export function Post({ post }) {
   return (
     <div>
       <span>{post.id}</span>
--- pages/contact-page.jsx
+++ pages/contact-page.jsx
@@ -1,4 +1,4 @@
-import Layout from "../components/Layout";
+import { Layout } from "../components/Layout";
 import { ContactCard } from "../components/ContactCard";
 
 export default function Contact() {
